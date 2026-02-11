c18686e3a05b7d382959573220e53d1de4dab46c
Rajdip <whitmanadon@gmail.com>
2026-02-11 22:59:23 +0530

    Add files via upload
    This is my first c programming with some important things

---
+++ bravo.c
@@ -0,0 +1,41 @@
+//CHapter 0
+#include<stdio.h>
+int main(){
+    int a;//Variable
+    
+    int harry_is_good; //no
+    float bloat = 1.4;
+    int value;
+    scanf("%d", &value);
+
+    printf("The Value of a is: %d\n", value);
+    //char duol = "sudo";
+    a = 6;  //innitialization
+    printf("hello world\n");
+    printf("sudooo\n");
+    printf("The OUtput OF The Programme is %d\n", a);
+    
+    
+
+
+
+    printf("size of int: %zu\n", sizeof(int));
+    //printf("duol\n");
+
+    return 0;
+
+    //Chapter 1
+
+
+}
+
+// no special character, numbers, dollar, pound, hashtag, () not allowed
+// 1. first character must be an alphabet
+// 2. no commas, blanks are allowed
+//3. no special symbol except "_" allowed
+//4. variable names are case sensitive(means if you write same variable it is calculated two difference variable by c)
+
+//boilerplate code
+
+// escape sequence characters .....
+
