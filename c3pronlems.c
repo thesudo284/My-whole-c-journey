6a0f829a1b52fceaddf1facf3f46c681e426d1b3
Rajdip <whitmanadon@gmail.com>
2026-02-14 19:32:03 +0530

    1st problems of chapter 3
    Let's goooo 🥹

---
+++ c3pronlems.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+	int a = 10;
+	if(a = 11)
+	printf("I am 11");
+	
+	else
+	printf("I am not 11");
+	
+	}
+	//if curly brackets did not start and end then the first statement will be true as per the clang rule i dont know this after i search on the gemini ai i know this
