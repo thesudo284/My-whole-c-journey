6af1202678d74e1adc93e386b4111fce2779b819
Rajdip <whitmanadon@gmail.com>
2026-02-16 18:28:43 +0530

    half chapter complete

---
+++ chapter4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int i = 0;
+    // while(i<120){
+    // printf("pkmb %d baar\n", i);
+    // i = i + 1;
+    // }
+    int x = 0;
+    while(x<=20){
+        if(x>=10){
+            printf("Value %d\n", x);
+            
+        }
+        x++;
+    }
+    return 0;
+}
