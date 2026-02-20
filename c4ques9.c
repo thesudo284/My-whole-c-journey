137be3694137698c0803bd5b4f1aab9056763842
TheSudo <whitmanadon@gmail.com>
2026-02-20 20:35:59 +0530

    Chapter 4 And Question Number 9(11 Actually).
    ☠️☠️

---
+++ c4ques9.c
@@ -0,0 +1,21 @@
+// Implement 10 using other types of loops.
+
+//First of all i cant understand this fooooock**g question
+//but lets do i dont know
+
+#include<stdio.h>
+
+int main(){
+    int a = 1;
+
+    while (a<=10)
+    {
+        
+        a++;
+
+        if(a==10){
+            printf("%d\n", a);
+        }
+    }
+    
+}
